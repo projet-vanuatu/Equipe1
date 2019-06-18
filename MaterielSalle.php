b8d569a5740360f23108de77070cad9bf79dd54a
21505107 <21505107@a18478.ad.ut-capitole.fr>
2019-06-18 15:23:10 +0200

    version 0.6

---
+++ MaterielSalle.php
@@ -0,0 +1,15 @@
+<?php
+require 'connexion.php';
+//Fonction de recherche du materiel équipé dans la salle
+if(isset($_POST["ids"]) && !empty($_POST["ids"])){
+    $id=$_POST["ids"];
+    $conn= dbConnect();
+    $sql = "SELECT TypeMat, Etat_fonctionnement FROM MATERIELS WHERE IdS=$id";
+    $stmt = $conn->prepare($sql); 
+    $stmt->execute();
+    $resMa = $stmt->fetchAll(PDO::FETCH_ASSOC);
+    $_SESSION["Materiel"]=$resMa;
+}
+if(isset($_SESSION['Materiel'])){
+    print_r(utf8_encode(json_encode($_SESSION['Materiel'])));
+}
